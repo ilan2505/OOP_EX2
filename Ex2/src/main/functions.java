a18cc3d40bd8c3d8a138c301e4755bf39adea263
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-01 00:43:09 +0200

    Add files via upload

---
+++ Ex2/src/main/functions.java
@@ -0,0 +1,44 @@
+package main;
+import java.io.*;
+
+public class functions  {
+    //help function 1 -----------------------------------------------------------------------------
+    public static int count_lines(String file_name) {
+        int count = 0;
+        try {
+            File file = new File(file_name);
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            while (( reader.readLine()) != null) {
+                count++;
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return count;
+    }
+
+    //help function 2 ---------------------------------------------------------------------------
+    public static String create_files(int num_file) {
+        String fileName = "file_" + num_file + ".txt";
+        try {
+            File file = new File(fileName);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return fileName;
+    }
+
+    //help function 3 -----------------------------------------------------------------------------
+    public static void write_files(String file_name, int num_lines) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file_name));
+            for (int i = 0; i < num_lines; i++) {
+                writer.write("Hello World" + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
