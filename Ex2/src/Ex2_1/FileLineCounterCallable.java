e00207b39c3f62b5dca2e87ab0a05a668e59d95b
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-03 22:14:44 +0200

    Update FileLineCounterCallable.java

--- Ex2/src/Ex2_1/FileLineCounterCallable.java
+++ Ex2/src/Ex2_1/FileLineCounterCallable.java
@@ -7,15 +7,27 @@ import java.util.concurrent.Callable;
 
 import static Ex2_1.Ex2_1.getNumOfLines;
 
+/**
+ * This class is an implement of Callable that we need to use ThreadPool in our function number 4.
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
 public class FileLineCounterCallable implements Callable<Integer> {
-    private String fileName;
-    private int numOfLines;
+    private String fileName;        //name of the file
+    private int numOfLines;         //number of lines
 
+    /**
+     * Constructor
+     * @param fileName - Name of the file.
+     */
     public FileLineCounterCallable(String fileName) {
         this.fileName = fileName;
         this.numOfLines = 0;
     }
 
+    /**
+     * Call method for the forth function with ThreadPool.
+     * @return - count_lines(fileName).
+     */
     @Override
     public Integer call() throws Exception {
         return functions.count_lines(fileName);
