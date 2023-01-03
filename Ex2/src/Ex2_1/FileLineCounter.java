890b9688ff4596f1336c2668346408f8a2870214
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-03 22:13:01 +0200

    Update FileLineCounter.java

--- Ex2/src/Ex2_1/FileLineCounter.java
+++ Ex2/src/Ex2_1/FileLineCounter.java
@@ -2,18 +2,34 @@ package Ex2_1;
 import java.io.*;
 import java.util.Scanner;
 
+/**
+ * This class extends Thread that we need to use threads in our function number 3.
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
 public class FileLineCounter extends Thread {
-    private String fileName;
-    private int numOfLines;
+    private String fileName;       //name of the file
+    private int numOfLines;        //number of lines
 
+    /**
+     * Constructor
+     * @param fileName - Name of the file.
+     */
     public FileLineCounter(String fileName) {
         this.fileName = fileName;
         this.numOfLines = 0;
     }
 
+    /**
+     * Run method for the third function with threads.
+     */
     public void run() {
         this.numOfLines = functions.count_lines(fileName);
     }
+    
+    /**
+     * This method gives the num of lines into threads.
+     * @return - numOfLines.
+     */
     public int getNumOfLines() {
         return numOfLines;
     }
