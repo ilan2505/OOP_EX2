5330c574e774828db22d3f48c6eaf9b568328582
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-03 22:09:07 +0200

    Update Ex2_1.java

--- Ex2/src/Ex2_1/Ex2_1.java
+++ Ex2/src/Ex2_1/Ex2_1.java
@@ -5,11 +5,26 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 
+/**
+ * This class creates several text files and calculate the total number of lines in these files.
+ * We will use three methods to do this mission :
+ * 1. Normal method without Threads.
+ * 2. Method with Threads.
+ * 3. Method with ThreadPool.
+ * The class includes method main to check that everything is working properly.
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
+
 public class Ex2_1 {
-    //fonction qui crer un text avec un nombre aleatoire de lignes avec ecrit "Hello World" par ligne.
-    //n = nombre de fichier txt creer
-    //bound = nombre random de "hello word" dans le texte mais au max c'est bound (se compte en ligne)
-    //seed = garde un set defini qui donnera a chaque fois le meme resulat.
+    
+    /**
+     * This method creates n text files on disk and returns an array of the file names.
+     * In each line we write a sentence : "Hello World".
+     * @param n - A natural number representing the number of text files.
+     * @param seed - Seed for random. (a defined set that will give the same result each time)
+     * @param bound - Maximum number of lines in one file.
+     * @return files_names - An array of file names.
+     */
     public static String[] createTextFiles(int n, int seed, int bound){
         String[] files_names = new String[n];
         Random rand = new Random(seed);
@@ -21,7 +36,12 @@ public class Ex2_1 {
         return files_names;
     }
 
-    //fonction 2
+    /**
+     * This method gives the total number of lines in these files.
+     * This method doesn't use any thread.
+     * @param fileNames - An array that contains the file names.
+     * @return numOfLines - total number of lines in these files.
+     */
     public static int getNumOfLines(String[] fileNames){
         int numOfLines = 0;
         for (String file_name : fileNames) {
@@ -30,7 +50,12 @@ public class Ex2_1 {
         return numOfLines;
     }
 
-    //fonction 3
+    /**
+     * This method gives the total number of lines in these files.
+     * This method uses Threads.
+     * @param fileNames - An array that contains the file names.
+     * @return numOfLines - total number of lines in these files.
+     */
     public static int getNumOfLinesThreads(String[] fileNames) {
         FileLineCounter[] threads = new FileLineCounter[fileNames.length];
         int numOfLines = 0;
@@ -53,7 +78,12 @@ public class Ex2_1 {
         return numOfLines;
     }
 
-    //fonction 4
+    /**
+     * This method gives the total number of lines in these files.
+     * This method uses ThreadPool.
+     * @param fileNames - An array that contains the file names.
+     * @return numOfLines - total number of lines in these files.
+     */
     public static int getNumOfLinesThreadPool(String[] fileNames){
         int numOfLines = 0;
         ExecutorService pool = Executors.newFixedThreadPool(fileNames.length);
@@ -94,23 +124,27 @@ public class Ex2_1 {
 
 
     //----------------------------------MAIN-----------------------------------------------------
+    /**
+     * This main checks that everything is working properly with our 3 methods.
+     * We can count how long time it takes to run each method.
+     */
     public static void main(String[] args) {
         int start_time, end_time;
-        String file_names[] = createTextFiles(1000, 42, 500);
+        String file_names[] = createTextFiles(100, 42, 99999);
 
-        System.out.println("-----------------------Exemple 1-----------------------");
+        System.out.println("[Normal Example]");
         start_time = (int) System.currentTimeMillis();
         System.out.println("Number of lines : " + getNumOfLines(file_names));
         end_time = (int) System.currentTimeMillis();
-        System.out.println("Time in milliseconds : " + (end_time - start_time));
+        System.out.println("Time in milliseconds : " + (end_time - start_time) + "\n");
 
-        System.out.println("-----------------------Exemple 1 using threads-----------------------");
+        System.out.println("[Example using Threads]");
         start_time = (int) System.currentTimeMillis();
         System.out.println("Number of lines : " + getNumOfLinesThreads(file_names));
         end_time = (int) System.currentTimeMillis();
-        System.out.println("Time in milliseconds : " + (end_time - start_time));
+        System.out.println("Time in milliseconds : " + (end_time - start_time) + "\n");
 
-        System.out.println("-----------------------Exemple 1 using threads pool-----------------------");
+        System.out.println("[Example using Threads Pool]");
         start_time = (int) System.currentTimeMillis();
         System.out.println("Number of lines : " + getNumOfLinesThreadPool(file_names));
         end_time = (int) System.currentTimeMillis();
