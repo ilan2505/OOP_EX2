075b6790ddd09663a75f3005526130b7a11eb429
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-10 20:03:50 +0200

    Update FutureTaskComparator.java

--- Ex2/src/Ex2_2_Improved/FutureTaskComparator.java
+++ Ex2/src/Ex2_2_Improved/FutureTaskComparator.java
@@ -4,8 +4,18 @@ import Ex2_2.FutureTask;
 
 import java.util.Comparator;
 
+/**
+ * This class provides
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
 public class FutureTaskComparator<T> implements Comparator<FutureTaskAdapter<T>> {
 
+    /**
+     * This function Compares the task with the task for order.
+     * @param t1 - object that we compare
+     * @param t2 - object that we compare to the other.
+     * @return - a 0, -1 or 1  if the object is equal/less/greater to/than the specified object.
+     */
     @Override
     public int compare(FutureTaskAdapter<T> t1, FutureTaskAdapter<T> t2) {
         if (t1.getPriority() < t2.getPriority()){
