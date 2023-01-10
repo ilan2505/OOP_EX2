b416739d250009fbd389989c94accc4b768f630d
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-10 20:03:16 +0200

    Update FutureTaskAdapter.java

--- Ex2/src/Ex2_2_Improved/FutureTaskAdapter.java
+++ Ex2/src/Ex2_2_Improved/FutureTaskAdapter.java
@@ -3,9 +3,18 @@ package Ex2_2_Improved;
 
 import java.util.concurrent.FutureTask;
 
+/**
+ * This class provides
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
 public class FutureTaskAdapter<T> extends FutureTask {
 
     private Task<T> task;
+    
+    /**
+     * Constructor of the FutureTaskAdapter class.
+     * @param task
+     */
     public FutureTaskAdapter(Task<T> task)
     {
         super(task);
@@ -13,6 +22,10 @@ public class FutureTaskAdapter<T> extends FutureTask {
 
     }
 
+    /**
+     * This function
+     * @return task -
+     */
     public int getPriority()
     {
         return this.task.getPriority();
