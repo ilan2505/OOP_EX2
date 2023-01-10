6c1c65b437117dd71194e84d93dbd1f6f87cd02a
Ilan Meyer Souffir <55143087+ilan2505@users.noreply.github.com>
2023-01-10 20:05:09 +0200

    Update Task.java

--- Ex2/src/Ex2_2_Improved/Task.java
+++ Ex2/src/Ex2_2_Improved/Task.java
@@ -2,28 +2,56 @@ package Ex2_2_Improved;
 
 import java.util.concurrent.Callable;
 
+/**
+ * This class provides
+ * @author Jonatan Boritsky : 207254194,  Ilan Meyer Souffir : 342615648
+ */
 public class Task<T> implements Callable<T> {
     private Callable<T> task;
     private TaskType type;
 
+    /**
+     * This function creates a new task with the given task and the task type.
+     * @param CallableObject
+     * @param type
+     */
     private Task(Callable<T> CallableObject, TaskType type) {
         this.task = CallableObject;
         this.type = type;
     }
 
+    /**
+     * This function creates a task with the given task and the task type.
+     * @param callableObject
+     * @param type
+     * @return task - The task object
+     */
     public static <T> Task<T> createTask(Callable<T> callableObject, TaskType type) {
         Task<T> task = new Task<T>(callableObject, type);
         return task;
     }
 
+    /**
+     * This function creates a task with the given task.
+     * @param callableObject
+     * @return task - the new task object
+     */
     public static <T> Task<T> createTask(Callable<T> callableObject) {
         return Task.createTask(callableObject, TaskType.OTHER);
     }
 
+    /**
+     * This function get the priority value of the type.
+     * @return the priority value of the type.
+     */
     public int getPriority(){
         return type.getPriorityValue();
     }
 
+    /**
+     * This function calls the method for the task.
+     * @return task - the result of the task.
+     */
     @Override
     public T call() throws Exception {
         return task.call();
